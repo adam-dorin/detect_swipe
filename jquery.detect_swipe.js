dfef193eb576548296bc41fec091d859982424a3
Adam <adamoctav@gmail.com>
2018-07-05 11:07:48 +0300

    add support for passive listeners
    mainly because of chrome https://www.chromestatus.com/features/5707400183021568

--- jquery.detect_swipe.js
+++ jquery.detect_swipe.js
@@ -16,70 +16,85 @@
         // Browser globals
         factory(jQuery);
     }
-}(function($) {
+}(function ($) {
 
-  $.detectSwipe = {
-    version: '2.1.2',
-    enabled: 'ontouchstart' in document.documentElement,
-    preventDefault: true,
-    threshold: 20
-  };
+    $.detectSwipe = {
+        version: '2.1.2',
+        enabled: 'ontouchstart' in document.documentElement,
+        preventDefault: true,
+        threshold: 20
+    };
 
-  var startX,
-    startY,
-    isMoving = false;
+    var startX,
+        startY,
+        isMoving = false;
 
-  function onTouchEnd() {
-    this.removeEventListener('touchmove', onTouchMove);
-    this.removeEventListener('touchend', onTouchEnd);
-    isMoving = false;
-  }
-
-  function onTouchMove(e) {
-    if ($.detectSwipe.preventDefault) { e.preventDefault(); }
-    if(isMoving) {
-      var x = e.touches[0].pageX;
-      var y = e.touches[0].pageY;
-      var dx = startX - x;
-      var dy = startY - y;
-      var dir;
-      var ratio = window.devicePixelRatio || 1;
-      if(Math.abs(dx) * ratio >= $.detectSwipe.threshold) {
-        dir = dx > 0 ? 'left' : 'right'
-      }
-      else if(Math.abs(dy) * ratio >= $.detectSwipe.threshold) {
-        dir = dy > 0 ? 'up' : 'down'
-      }
-      if(dir) {
-        onTouchEnd.call(this);
-        $(this).trigger('swipe', dir).trigger('swipe' + dir);
-      }
+    function supportsPassiveListeners() {
+        var supportsPassiveOption = false
+        try {
+            var opts = Object.defineProperty({}, 'passive', {
+                get: function () {
+                    supportsPassiveOption = true;
+                }
+            });
+            window.addEventListener('test', null, opts);
+        } catch (e) { }
+        return supportsPassiveOption;
     }
-  }
 
-  function onTouchStart(e) {
-    if (e.touches.length == 1) {
-      startX = e.touches[0].pageX;
-      startY = e.touches[0].pageY;
-      isMoving = true;
-      this.addEventListener('touchmove', onTouchMove, false);
-      this.addEventListener('touchend', onTouchEnd, false);
+    function onTouchEnd() {
+        this.removeEventListener('touchmove', onTouchMove);
+        this.removeEventListener('touchend', onTouchEnd);
+        isMoving = false;
     }
-  }
 
-  function setup() {
-    this.addEventListener && this.addEventListener('touchstart', onTouchStart, false);
-  }
+    function onTouchMove(e) {
+        if ($.detectSwipe.preventDefault) { e.preventDefault(); }
+        if (isMoving) {
+            var x = e.touches[0].pageX;
+            var y = e.touches[0].pageY;
+            var dx = startX - x;
+            var dy = startY - y;
+            var dir;
+            var ratio = window.devicePixelRatio || 1;
+            if (Math.abs(dx) * ratio >= $.detectSwipe.threshold) {
+                dir = dx > 0 ? 'left' : 'right'
+            }
+            else if (Math.abs(dy) * ratio >= $.detectSwipe.threshold) {
+                dir = dy > 0 ? 'up' : 'down'
+            }
+            if (dir) {
+                onTouchEnd.call(this);
+                $(this).trigger('swipe', dir).trigger('swipe' + dir);
+            }
+        }
+    }
 
-  function teardown() {
-    this.removeEventListener('touchstart', onTouchStart);
-  }
+    function onTouchStart(e) {
+        if (e.touches.length == 1) {
+            startX = e.touches[0].pageX;
+            startY = e.touches[0].pageY;
+            isMoving = true;
+            this.addEventListener('touchmove', onTouchMove, supportsPassiveListeners() ? { passive: true } : false);
+            this.addEventListener('touchend', onTouchEnd, supportsPassiveListeners() ? { passive: true } : false);
+        }
+    }
 
-  $.event.special.swipe = { setup: setup };
+    function setup() {
+        this.addEventListener && this.addEventListener('touchstart', onTouchStart, supportsPassiveListeners() ? { passive: true } : false);
+    }
 
-  $.each(['left', 'up', 'down', 'right'], function () {
-    $.event.special['swipe' + this] = { setup: function(){
-      $(this).on('swipe', $.noop);
-    } };
-  });
+    function teardown() {
+        this.removeEventListener('touchstart', onTouchStart);
+    }
+
+    $.event.special.swipe = { setup: setup };
+
+    $.each(['left', 'up', 'down', 'right'], function () {
+        $.event.special['swipe' + this] = {
+            setup: function () {
+                $(this).on('swipe', $.noop);
+            }
+        };
+    });
 }));
